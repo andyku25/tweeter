33bf86f72339de0469d17b431377c429d5cbd2dd
Andrew <andrew.ku93@gmail.com>
2021-02-24 20:04:14 +0000

    implemented 2 functions to create an element and display test data. Added ajax post handling of data/tweet

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,8 +4,103 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// append data to the articles section
+const createTweetElement = tweetObj => {
+  return `<article class="tweet">
+    <header>
+      <div class="tweet-profile-details">
+        <img class="tweet-img" src="${tweetObj.user.avatars}" alt="${tweetObj.user.handle}">
+        <span>${tweetObj.user.name}</span>
+      </div> 
+      <div class="username">
+        ${tweetObj.user.handle}
+      </div>
+    </header>
+    <div class="tweet-content">
+      ${tweetObj.content.text}
+    </div>
+    <hr class="tweet-row">
+    <footer>
+      <small>${tweetObj.created_at}</small>
+      <div>
+        <i class="far fa-flag"></i>
+        <i class="fas fa-retweet"></i>
+        <i class="far fa-heart"></i>
+      </div>
+    </footer>
+  </article>`;
+};
+
+const renderTweets = tweetObjs => {
+  let allTweets = "";
+  for (const tweetObj of tweetObjs) {
+    allTweets += createTweetElement(tweetObj);
+  }
+  $("#tweets-container").append(allTweets);
+};
+
 $(document).ready(function() {
-  $("#tweets").on("load", function() {
+  // empty the tweets-container
+  $("tweets-container").empty();
+
+
+  // Handling new post  submissions
+  $("#new-tweet").on("submit", function(event) {
+    event.preventDefault();
+    let $textarea = $(this).find("textarea");
+    const $tweetContent = $textarea.serialize();
+    console.log($tweetContent);
+
+    $.ajax({
+      url: "/tweets",
+      method: "POST",
+      data: $tweetContent
+    })
+    .done((data) => {
+      console.log(data);
+    })
+    .fail((err) => {
+      console.log(err);
+    })
+    .always(() => {
+      console.log("Complete");
+    })
     
-  })
+    // make the textarea box back to blank once form is submitted
+    $textarea.val("");
+  });
+
+  // Test Method: add tweetsObjs from json file
+  const tweetsData = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png",
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1614014982252
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd"
+      },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1614101382252
+    }
+  ];
+
+  renderTweets(tweetsData);
+  
+  // Future Method: get tweet object data via ajax
+  // $.ajax({
+  //   url,
+  //   method: "GET"
+  // })  
 });
