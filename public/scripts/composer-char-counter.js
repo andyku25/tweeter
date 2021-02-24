cfad3ddf064e34beea6de6c6fd69452f8d697d92
Andrew <andrew.ku93@gmail.com>
2021-02-24 01:16:24 +0000

    create styling mockup of tweets/articles section

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,18 +1,19 @@
 $(document).ready(function() {
   $("#tweet-text").on("input", function() {
-    console.log(this);
+    // console.log(this);
     const textArea = $(this).val();
     const $parent = $(this).closest("div");
     const $counterOutput = $parent.find(".counter");
-    
-    let charCount = 140 - textArea.length;
+
+    const charLimit = 140
+    let charCount = charLimit - textArea.length;
 
     if (charCount < 0) {
       $counterOutput.addClass("negative-count");
     } else {
       $counterOutput.removeClass("negative-count");
     }
-    
+
     $counterOutput.text(charCount);
   })
 }); // document ready
